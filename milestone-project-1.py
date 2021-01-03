c74e7e6c7798c9c04a1f459a55decae8e5ba068e
ottomattas <otto.mattas@gmail.com>
2021-01-03 20:39:14 +0100

    Add user input method and verification

--- milestone-project-1.py
+++ milestone-project-1.py
@@ -8,3 +8,26 @@ __version__ = "0.1"
 __maintainer__ = "Otto Mättas"
 __email__ = "otto.mattas@eesti.ee"
 __status__ = "Development"
+
+def user_choice():
+    
+    choice ='WRONG'
+    within_range = False
+    
+    while choice.isdigit() == False or within_range == False:
+        
+    
+    
+        choice = input("Please enter a number (0-10): ")
+        
+        if choice.isdigit() == False:
+            print("Sorry that is not a digit!")
+            
+        if choice.isdigit() == True:
+            if int(choice) in range(0,10):
+                within_range = True
+            else:
+                within_range = False
+        
+    
+    return int(choice)
