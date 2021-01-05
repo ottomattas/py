b91e7979b07073617d1d6036fc62ca3db2d0a5ea
ottomattas <otto.mattas@gmail.com>
2021-01-05 19:47:26 +0100

    Adjust copyright to match the project scope better

--- milestone-project-1.py
+++ milestone-project-1.py
@@ -2,7 +2,7 @@
 """This is a simple interactive Tic-tac-toe game."""
 
 __author__ = "Otto Mättas"
-__copyright__ = "Copyright 2021, The Python Project"
+__copyright__ = "Copyright 2021, The Python Problems"
 __license__ = "MIT"
 __version__ = "0.1"
 __maintainer__ = "Otto Mättas"
