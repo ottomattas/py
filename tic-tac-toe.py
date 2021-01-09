f8fdd08e68b385c2bd322ac1710d6eac6da74619
ottomattas <otto.mattas@gmail.com>
2021-01-09 14:10:18 +0100

    Define the full board checking function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -76,3 +76,12 @@ def space_check(board, position):
         return True
     else:
         return False
+
+# DEFINE THE FULL BOARD CHECKING FUNCTION
+def full_board_check(board):
+    if board[1] and board[2] and board[3] and \
+       board[4] and board[5] and board[6] and \
+       board[7] and board[8] and board[9] != ' ':
+        return True
+    else:
+        return False
