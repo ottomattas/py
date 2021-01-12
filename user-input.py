9e59657e9057980bb791f468b36d6fe4210f8f3d
ottomattas <otto.mattas@gmail.com>
2021-01-12 15:15:57 +0100

    Add comments

--- user-input.py
+++ user-input.py
@@ -9,23 +9,29 @@ __maintainer__ = "Otto Mättas"
 __email__ = "otto.mattas@eesti.ee"
 __status__ = "Development"
 
+# DEFINE THE USER CHOICE INPUT FUNCTION
 def user_choice():
     
+    # DEFINE VARIABLES
     choice = ' '
     within_range = False
 
+    # KEEP ASKIGN FOR USER CHOICE INPUT
     while choice.isdigit() == False or within_range == False:
          
         choice = input('Please enter a number (0-10): ')
         
+        # VALIDATE INPUT AS DIGIT
         if choice.isdigit() == False:
             print('Sorry, that is not a digit!')
-            
+        
+        # VALIDATE INPUT RANGE
         if choice.isdigit() == True:
             if int(choice) in range(0,10):
                 within_range = True
             else:
                 within_range = False
-        
+
+    # RETURN THE USER CHOICE AS AN INT
     return int(choice)
     
