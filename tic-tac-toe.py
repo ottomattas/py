c46b3f97317d1db26b861764889ce3c2e8ed4041
Otto Mättas <otto.mattas@gmail.com>
2021-01-13 22:27:48 +0100

    Nudge the version after final function definitions

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,7 +4,7 @@
 __author__ = "Otto Mättas"
 __copyright__ = "Copyright 2021, The Python Problems"
 __license__ = "MIT"
-__version__ = "0.1"
+__version__ = "0.2"
 __maintainer__ = "Otto Mättas"
 __email__ = "otto.mattas@eesti.ee"
 __status__ = "Development"
