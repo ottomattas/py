1b13e7aad2251a81777054904d021e433fa89f24
ottomattas <otto.mattas@gmail.com>
2021-02-06 15:40:20 +0100

    Initialise new class example from the course

---
+++ class-example-banking.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""This is a simple bank account class
+with some attributes and methods to manipulate it."""
+
+__author__ = "Otto Mättas"
+__copyright__ = "Copyright 2021, The Python Problems"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Otto Mättas"
+__email__ = "otto.mattas@eesti.ee"
+__status__ = "Development"
