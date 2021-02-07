c6f45d6b0bc8c174dbb7ebcbad5fdb896ddf8bc9
ottomattas <otto.mattas@gmail.com>
2021-02-07 09:21:43 +0100

    Initialise myprogram

---
+++ Modules_Packages_Example/myprogram.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""This is a simple program to show examples
+on how to use modules and packages."""
+
+__author__ = "Otto Mättas"
+__copyright__ = "Copyright 2021, The Python Problems"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Otto Mättas"
+__email__ = "otto.mattas@eesti.ee"
+__status__ = "Development"
