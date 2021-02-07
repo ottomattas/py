1c7cf4747faf56dd2ba95c324e7c4ec010b0db3d
ottomattas <otto.mattas@gmail.com>
2021-02-07 09:22:11 +0100

    Initialise mymodule and call it

--- Modules_Packages_Example/mymodule.py
+++ Modules_Packages_Example/mymodule.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-"""This is a simple program to show examples
-on how to use modules and packages."""
+"""This is a simple module."""
 
 __author__ = "Otto Mättas"
 __copyright__ = "Copyright 2021, The Python Problems"
@@ -9,6 +8,3 @@ __version__ = "0.1"
 __maintainer__ = "Otto Mättas"
 __email__ = "otto.mattas@eesti.ee"
 __status__ = "Development"
-
-# IMPORT METHOD FROM MODULE
-from mymodule import my_func
