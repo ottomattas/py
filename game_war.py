942685026f7b49923eb218f0a17852b1ef86efd7
ottomattas <otto.mattas@gmail.com>
2021-02-20 15:18:13 +0100

    Define values dict for giving the rank a value

--- game_war.py
+++ game_war.py
@@ -13,6 +13,11 @@ __status__ = "Testing"
 # GLOBAL VARIABLES #
 ####################
 
+# DEFINE VALUES DICT FOR GIVING THE RANK A VALUE
+values = {'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,
+        'Seven':7,'Eight':8,'Nine':9,'Ten':10,
+        'Jack':11,'Queen':12,'King':13,'Ace':14}
+
 #####################
 # CLASS DEFINITIONS #
 #####################
