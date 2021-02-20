5f9e102f24c2490de430f75e390cbc8dc1a198e7
ottomattas <otto.mattas@gmail.com>
2021-02-20 14:15:25 +0100

    Fix version

--- game_war.py
+++ game_war.py
@@ -4,7 +4,7 @@
 __author__ = "Otto Mättas"
 __copyright__ = "Copyright 2021, The Python Problems"
 __license__ = "MIT"
-__version__ = "0.9"
+__version__ = "0.1"
 __maintainer__ = "Otto Mättas"
 __email__ = "otto.mattas@eesti.ee"
 __status__ = "Testing"
@@ -14,6 +14,7 @@ __status__ = "Testing"
 #####################
 
 # CARD CLASS
+
 
 # DECK CLASS
 
